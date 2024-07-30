ba120010df3b76e383312a067b4ef139f246fe2f
Yu Chen, Yang <ycy.tai@gmail.com>
2024-07-30 22:07:38 +0800

    fix: use string for dunder all

--- fmd/objs/__init__.py
+++ fmd/objs/__init__.py
@@ -1,4 +1,4 @@
 from fmd.objs.etf import Etf, EtfManager
 from fmd.objs.stock import Stock, StockManager
 
-__all__ = [StockManager, Stock, EtfManager, Etf]
+__all__ = ['StockManager', 'Stock', 'EtfManager', 'Etf']
